56fc1df71bc10e627564de16f5f71c8a344e0bfd
Lali576 <lali576@hotmail.com>
2018-03-19 17:09:01 +0100

    Init java project

---
+++ src/main/java/hu/elte/wr14yr/musicportal/MusicportalApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.wr14yr.musicportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicportalApplication.class, args);
+	}
+}
