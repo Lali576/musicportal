25c7b2d5c099853c4893a543ad2834660865c175
Lali576 <lali576@hotmail.com>
2018-04-24 21:53:18 +0200

    Added angular components and route module

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,11 +3,39 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MenuComponent } from './menu/menu.component';
+import { AlbumEditComponent } from './album-edit/album-edit.component';
+import { AlbumListComponent } from './album-list/album-list.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { PlaylistListComponent } from './playlist-list/playlist-list.component';
+import { PlaylistEditComponent } from './playlist-edit/playlist-edit.component';
+import { PlaylistDetailComponent } from './playlist-detail/playlist-detail.component';
+import { SongEditComponent } from './song-edit/song-edit.component';
+import { SongDetailComponent } from './song-detail/song-detail.component';
+import { SongListComponent } from './song-list/song-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserEditComponent,
+    UserDetailComponent,
+    LoginComponent,
+    MainPageComponent,
+    MenuComponent,
+    AlbumEditComponent,
+    AlbumListComponent,
+    AlbumDetailComponent,
+    PlaylistListComponent,
+    PlaylistEditComponent,
+    PlaylistDetailComponent,
+    SongEditComponent,
+    SongDetailComponent,
+    SongListComponent
   ],
   imports: [
     BrowserModule
