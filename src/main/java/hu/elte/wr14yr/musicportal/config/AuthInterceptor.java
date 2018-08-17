5db93bb9a6c9706ba5a9bbf46ae07639279edfeb
Lali576 <lali576@hotmail.com>
2018-08-17 16:20:22 +0200

    Updated auth interceptor with logger

--- src/main/java/hu/elte/wr14yr/musicportal/config/AuthInterceptor.java
+++ src/main/java/hu/elte/wr14yr/musicportal/config/AuthInterceptor.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
 public class AuthInterceptor extends HandlerInterceptorAdapter {
@@ -20,16 +22,22 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
     @Autowired
     private UserService userService;
 
+    private Logger logger = Logger.getLogger(AuthInterceptor.class.getName());
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.log(Level.INFO, "Authentication interceptor triggered");
+
         List<User.Role> routeRoles = getRoles((HandlerMethod) handler);
         User user = (userService.isLoggedIn()) ? userService.getLoggedInUser() : null;
 
         if(routeRoles.isEmpty() || routeRoles.contains(User.Role.GUEST)) {
+            logger.log(Level.INFO, "Access to current route is free");
             return true;
         }
 
         if(userService.isLoggedIn() && routeRoles.contains(user.getRole())) {
+            logger.log(Level.INFO, "Access to current route is granted");
             return true;
         }
 
