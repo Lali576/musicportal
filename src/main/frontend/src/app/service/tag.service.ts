9e935923512680d2d3a4dfc6437e3f67b02afc4d
Lali576 <lali57612@gmail.com>
2018-11-06 22:37:35 +0100

    Added tag service for angular client

---
+++ src/main/frontend/src/app/service/tag.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Album} from "../model/album";
+import {AlbumTag} from "../model/tags/albumtag";
+import {Song} from "../model/song";
+import {SongTag} from "../model/tags/songtag";
+import {Playlist} from "../model/playlist";
+import {PlaylistTag} from "../model/tags/playlisttag";
+import {User} from "../model/user";
+import {UserTag} from "../model/tags/usertag";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getTagsByAlbum(album: Album): Promise<AlbumTag[]> {
+    const formData = new FormData();
+    formData.append("album", JSON.stringify(album));
+    return this.http.post<AlbumTag[]>(
+      'api/tag/list-by-album',
+      formData
+    ).toPromise();
+  }
+
+  getTagsBySong(song: Song): Promise<SongTag[]> {
+    const formData = new FormData();
+    formData.append("song", JSON.stringify(song));
+    return this.http.post<SongTag[]>(
+      'api/tag/list-by-song',
+      formData
+    ).toPromise();
+  }
+
+  getTagsByPlaylist(playlist: Playlist): Promise<PlaylistTag[]> {
+    const formData = new FormData();
+    formData.append("playlist", JSON.stringify(playlist));
+    return this.http.post<PlaylistTag[]>(
+      'api/tag/list-by-playlist',
+      formData
+    ).toPromise()
+  }
+
+  getTagsByUser(id: number): Promise<UserTag[]> {
+    return this.http.get<UserTag[]>(`api/tag/list-by-user/${id}`).toPromise();
+  }
+}
